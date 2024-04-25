678174376059ad75b4ecb47c105f70a2ef0d6e24
Miguel Mayans <miguelmayansv@gmail.com>
2024-04-25 13:54:59 +0200

    fix: alt image, footer

--- src/components/ImgCarousel.tsx
+++ src/components/ImgCarousel.tsx
@@ -14,28 +14,50 @@ export default function ImgCarousel() {
           effect={"flip"}
           grabCursor={true}
           pagination={true}
+          autoplay={true}
           className="mySwiper"
         >
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/niodbwhmzhouuepvi9ju" />
+            <img
+              src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/niodbwhmzhouuepvi9ju"
+              alt="Patatas y Batatas"
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/zrkbkrqdh6gj8ypccrvf" />
+            <img
+              src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/zrkbkrqdh6gj8ypccrvf"
+              alt="Bento Bowl"
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/cbundkf6t9qwfoftxlz8" />
+            <img
+              src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/cbundkf6t9qwfoftxlz8"
+              alt="Dumplings Ibéricos"
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/gllmnkyun3kqiiafjzvn" />
+            <img
+              src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/gllmnkyun3kqiiafjzvn"
+              alt="Taco de Salmón"
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/ynga7coxcmspxrykzirv" />
+            <img
+              src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/ynga7coxcmspxrykzirv"
+              alt="Croquetas de Cecina y Trufa"
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/o4loxpw5iqxbjfppcxag" />
+            <img
+              src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/o4loxpw5iqxbjfppcxag"
+              alt="Cafés Patio"
+            />
           </SwiperSlide>
           <SwiperSlide>
-            <img src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/tqgc45mh85jmkxigycge" />
+            <img
+              src="https://res.cloudinary.com/du13ofsms/image/upload/f_auto,q_auto/v1/Patio-Webpage/tqgc45mh85jmkxigycge"
+              alt="Sony en la Barra"
+            />
           </SwiperSlide>
         </Swiper>
       </section>
